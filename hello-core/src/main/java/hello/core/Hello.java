8d465c44b6059454e6bc314c96603caaa16b1139
gregwhitaker <gwhitake@gmail.com>
2019-11-03 07:18:57 -0800

    Doing work

--- hello-core/src/main/java/hello/core/Hello.java
+++ hello-core/src/main/java/hello/core/Hello.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Hello {
 
-    public String sayHello(String name) {
+    public static String sayHello(String name) {
         if (StringUtils.isNotEmpty(name)) {
             return "Hello " + StringUtils.capitalize(name) + "!";
         } else {
---
+++ hello-service/src/main/java/hello/service/controller/HelloController.java
@@ -0,0 +1,16 @@
+package hello.service.controller;
+
+import hello.core.Hello;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> sayHello(@RequestParam("name") String name) {
+        return ResponseEntity.ok(Hello.sayHello(name));
+    }
+}
